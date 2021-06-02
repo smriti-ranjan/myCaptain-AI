842f1c6edef9d1fa2a88bd5203cb6b2337b755be
smriti-ranjan <76141028+smriti-ranjan@users.noreply.github.com>
2021-06-02 13:26:27 +0530

    Updated the changes in the code

--- Fibonacci Series.py
+++ Fibonacci Series.py
@@ -1,4 +1,3 @@
-
 """The Fibonacci numbers are the numbers in the following integer sequence.
 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, …….. In mathematical terms,
 the sequence Fn of Fibonacci numbers is defined by the recurrence relation."""
@@ -11,10 +10,11 @@ f0 = 0
 f1 = 1
 count=0
 
-if n<0:
+if n<=0:
     print("Invalid Input!")
 elif n==1:
-    print("Fibonacci Series: " + n)
+    print("Fibonacci Series: ")
+    print(f0)
 else:
     print("Fibonacci Series: ")
     while count<n:
